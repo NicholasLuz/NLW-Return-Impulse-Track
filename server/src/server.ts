4bb772303bd330f8f828c1a87c894a6c61882430
Nicholas Luz <nicholas.s.luz@gmail.com>
2022-05-18 17:06:16 -0300

    fix CORS error

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,10 +6,16 @@ import bodyParser from 'body-parser';
 const app = express();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://nlw-return-impulse-track.vercel.app")
-    app.use(cors());
-    next();
-});
+    const allowedOrigins: string[] = ['https://nlw-return-impulse-track.vercel.app/', 'http://localhost:3000', 'http://192.168.15.98:3000', 'https://nlw-return-impulse-track-nicholasluz.vercel.app/', 'https://nlw-return-impulse-track-git-main-nicholasluz.vercel.app/'];
+    const origin: string = req.headers.origin!;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    return next();
+  });
+
 
 app.use(bodyParser.json({limit: '1mb'}))
 

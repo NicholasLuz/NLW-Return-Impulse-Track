6ab37dcd63d8b6334d4f70e083060362edec8327
Nicholas Luz <nicholas.s.luz@gmail.com>
2022-05-18 17:14:53 -0300

    add other domain to CORS allowed origins

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 const app = express();
 
 app.use((req, res, next) => {
-    const allowedOrigins: string[] = ['https://nlw-return-impulse-track.vercel.app/', 'http://localhost:3000', 'http://192.168.15.98:3000', 'https://nlw-return-impulse-track-nicholasluz.vercel.app/', 'https://nlw-return-impulse-track-git-main-nicholasluz.vercel.app/'];
+    const allowedOrigins: string[] = ['https://nlw-return-impulse-track.vercel.app', 'https://nlw-return-impulse-track.vercel.app/', 'http://localhost:3000', 'http://192.168.15.98:3000', 'https://nlw-return-impulse-track-nicholasluz.vercel.app/', 'https://nlw-return-impulse-track-git-main-nicholasluz.vercel.app/'];
     const origin: string = req.headers.origin!;
     if (allowedOrigins.includes(origin)) {
          res.setHeader('Access-Control-Allow-Origin', origin);
